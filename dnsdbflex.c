0a127a32e8092e4cea5bbf59c35347207d2c29cb
David Waitzman <djw@fsi.io>
2020-08-18 08:41:04 -0400

    Fix some static analysis issues

--- dnsdbflex.c
+++ dnsdbflex.c
@@ -295,7 +295,6 @@ main(int argc, char *argv[]) {
 	argc -= optind;
 	if (argc != 0)
 		usage("there are no non-option arguments to this program");
-	argv = NULL;
 
 	if (qd.value == NULL)
 		usage("Need to provide a --regex or --glob option and"
@@ -595,6 +594,7 @@ read_configs(void) {
 
 		wordexp(*conf, &we, WRDE_NOCMD);
 		cf = strdup(we.we_wordv[0]);
+		assert(cf != NULL);
 		wordfree(&we);
 		if (access(cf, R_OK) == 0) {
 			DEBUG(1, true, "conf found: '%s'\n", cf);
@@ -765,7 +765,6 @@ query_launcher(qdesc_ct qdp, writer_t writer) {
 	CREATE(query, sizeof(struct query));
 	query->writer = writer;
 	query->qd = *qdp;
-	writer = NULL;
 	query->writer->query = query;
 	query->command = makepath(qdp);
 
